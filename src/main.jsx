822c1077e52eb756e78ce75afe5c27ba4f2a1eb6
kevjmay <kevinjohnmyburgh@icloud.com>
2024-10-12 18:05:13 +0100

    rename file

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Bundlr from './Bundlr.jsx'
-import Email from './Test.jsx'
+import Email from './Email.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
