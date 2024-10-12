7d66a61f33106bee3c41515a2fe3f92ceb3e10ce
kevjmay <kevinjohnmyburgh@icloud.com>
2024-10-12 18:03:15 +0100

    add email form submission

---
+++ src/Test.jsx
@@ -0,0 +1,13 @@
+function Email() {
+    return (
+
+        <div style={{ display: "flex", alignItems: "center", justifyContent: "center", gap: "8px"}}>
+            <h3>Enter email: </h3>
+            <form>
+                <input type="text" />    
+            </form> 
+        </div>
+    );
+}
+
+export default Email;
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Bundlr from './Bundlr.jsx'
+import Email from './Test.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Bundlr />
+    <Email />
   </StrictMode>,
 )
