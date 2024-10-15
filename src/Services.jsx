4631b97576763e20b040412029da4c49d197d79a
kevjmay <kevinjohnmyburgh@icloud.com>
2024-10-15 19:28:51 +0100

    Implement keys in services list

--- src/Services.jsx
+++ src/Services.jsx
@@ -1,34 +1,20 @@
-function ServiceList(props) {
-   
-    if (!props.services) {
-        return <div>Loading services...</div>;
-    }
+const services = [
+    {serviceName: "Electric", id: crypto.randomUUID()},
+    {serviceName: "Gas", id: crypto.randomUUID()},
+    {serviceName: "Water", id: crypto.randomUUID()},
+    {serviceName: "TV License", id: crypto.randomUUID()},
+    {serviceName: "Broadband", id: crypto.randomUUID()}
+];
 
-    if (props.services.length === 0) {
-        return <div>There are no services listed at this time.</div>;
-    }
-
+function Services() {
     return (
-
         <ul>
-            {props.services.map((service) => {
-                return <li key={service}>{service}</li>;
-            })}
+            {services.map((service) => (
+                <li key={service.id}>{service.serviceName}</li>
+            ))}
         </ul>
     );
 
-}
-
-function Services() {
-
-    const services = ["Electric", "Gas", "Water", "TV License", "Broadband"];
-
-    return  (
-        <div>
-            <h1>Services: </h1>
-            <ServiceList services={services}/>
-        </div>
-    );
 }
 
 export default Services;
